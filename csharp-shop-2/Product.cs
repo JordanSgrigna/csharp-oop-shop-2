a6da52ec1ba0214a061cad70b0ed9f38654f9030
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-20 14:38:15 +0200

    Add project files.

---
+++ csharp-shop-2/Product.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_shop_2
+{
+	public class Product
+	{
+		//ATTRIBUTES + PROPERTIES
+		private string code;
+		public string Code
+		{
+			get
+			{
+				return this.code;
+			}
+		}
+
+		private string name;
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				this.name = value;
+			}
+		}
+
+		private string description;
+		public string Description
+		{
+			get
+			{
+				return this.description;
+			}
+			set
+			{
+				this.description = value;
+			}
+		}
+
+		private float price;
+		public float Price
+		{
+			get
+			{
+				return this.price;
+			}
+			set
+			{
+				this.price = value;
+			}
+		}
+
+		private int tax;
+		public int Tax
+		{
+			get
+			{
+				return this.tax;
+			}
+			set
+			{
+				this.tax = value;
+			}
+		}
+		private Category category;
+		public Category Category
+		{
+			get
+			{
+				return this.category;
+			}
+
+		}
+
+		//CONSTRUCTOR
+		public Product(string name, string description, float price, string categoryName, int tax = 22)
+		{
+			this.name = name;
+			this.description = description;
+			this.code = getCodePadLeft8();
+			this.price = price;
+			this.tax = tax;
+			this.category = new Category(categoryName);
+		}
+
+		//METHODS
+		private string getCodePadLeft8()
+		{
+			Random rnd = new Random();
+			int numeroCifre = rnd.Next(1, 9);
+
+			int codeInt = 0;
+
+			switch (numeroCifre)
+			{
+				case 1:
+					codeInt = rnd.Next(1, 10);
+					break;
+				case 2:
+					codeInt = rnd.Next(10, 100);
+					break;
+				case 3:
+					codeInt = rnd.Next(100, 1000);
+					break;
+				case 4:
+					codeInt = rnd.Next(1000, 10000);
+					break;
+				case 5:
+					codeInt = rnd.Next(10000, 1000000);
+					break;
+				case 6:
+					codeInt = rnd.Next(100000, 1000000);
+					break;
+				case 7:
+					codeInt = rnd.Next(1000000, 10000000);
+					break;
+				case 8:
+					codeInt = rnd.Next(10000000, 100000000);
+					break;
+
+			}
+
+			string codeString = codeInt.ToString();
+			string codeStringPadded = codeString.PadLeft(8, '0');
+			return codeStringPadded;
+
+		}
+
+		public string getProductInfo()
+		{
+			string infoProduct =
+			$"Nome: {this.name} \n" +
+			$"Descrizione: {this.description} \n" +
+			$"Codice: {this.code} \n" +
+			$"Prezzo: {this.price} \n" +
+			$"Categoria: \n" +
+			$"{category.getCategoryInfo()}";
+
+			return infoProduct;
+		}
+	}
+}
---
+++ csharp-shop-2/Water.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_shop_2
+{
+	public class Water : Product
+	{
+		//ATTRIBUTES
+		private string name;
+		private string brand;
+		private int maxCapacity;
+		private float pH;
+		private string source;
+
+		//CONSTRUCTOR
+		public Water(string name, string brand, int maxCapacity, float pH, string source) : base(name)
+		{
+			this.brand = brand;
+			this.maxCapacity = maxCapacity;
+			this.pH = pH;
+			this.source = source;
+		}
+
+
+
+		//GETTERS
+		public string GetName()
+		{
+			return this.name;
+		}
+		
+		public string GetBrand()
+		{
+			return this.brand;
+		}
+
+		public int MaxCapacity()
+		{
+			return this.maxCapacity;
+		}
+
+		public float GetpH()
+		{
+			return this.pH;
+		}
+
+		public string getSource()
+		{
+			return this.source;
+		}
+
+		//SETTERS
+		public void setBrand(string newBrand)
+		{
+			this.brand = newBrand;
+		}
+		
+	}
+}
